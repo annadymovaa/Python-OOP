6da004acad68734428de1aebc3423af8bef381e5
annadymovaa <aanadumova@hse.edu.ru>
2026-04-21 16:26:24 +0500

    changed the classes to more logical structure

--- src/analytics.py
+++ src/analytics.py
@@ -5,109 +5,12 @@ import requests
 import os
 import token
 
-class Research():
+logging.basicConfig(level=logging.DEBUG, filename='analytics.log', format='%(asctime)s %(message)s')
+
+class DataLoader():
     def __init__(self, filepath, has_header=True):
         self.filepath = filepath
         self.has_header = has_header
-
-    logging.basicConfig(level=logging.DEBUG, filename='analytics.log', format='%(asctime)s %(message)s')
-
-    class Calculations():
-        def __init__(self, data):
-            self.data = data
-
-        def counts(self):
-            self.heads = 0
-            self.tails = 0
-            for pair in self.data:
-                if pair[0] == 1:
-                    self.heads += 1
-                else:
-                    self.tails += 1
-            logging.debug('Calculating the counts of heads and tails')
-
-        def fractions(self):
-            self.counts()
-            summa = self.heads + self.tails
-            self.heads_perc = round(self.heads/summa, 4)
-            self.tails_perc = 1 - self.heads_perc
-            logging.debug('Calculating the fractions of heads and tails')
-
-
-    class Analytics(Calculations):
-        def predict_random(self, number: int):
-            predictions = list()
-            for i in range(number):
-                heads = randint(0, 1)
-                pair = [heads, 1 - heads]
-                predictions.append(pair)
-            logging.debug(f'Predicting {number} tosses')
-            return predictions
-
-        def predict_last(self):
-            last_pair = self.data[-1]
-            logging.debug('Predicting based on the last toss')
-            return last_pair
-
-        def save_file(self, data, file_name, extension='txt'):
-            filename = file_name + '.' + extension
-            with open(filename, 'w', encoding='utf-8') as file:
-                file.write(data)
-            logging.debug(f'Saving the data in a file named {filename}')
-            return filename
-
-        def num_to_word(self, num: int):
-            ans = num
-            if num >= 0 and num <= 10:
-                ans = config.numbers[num]
-            logging.debug('Turning numbers into words (0-10 works, the rest stay numbers)')
-            return ans
-
-    def get_message(self, filename):
-        message = "The report hasn't been created due to an error"
-        if os.path.isfile(filename) and os.path.getsize(filename) > 0:
-            message = "The report has been successfully created"
-        logging.debug('Generating a message for the TG channel')
-        return message
-
-    def send_message(self, filename):
-        message = self.get_message(filename)
-
-        url = f"https://api.telegram.org/bot{token.token}/sendMessage"
-        send_inf = {
-            'chat_id': token.chat_id,
-            'text': message
-            }
-        response = requests.post(url, data=send_inf)
-        logging.debug('Sending a message to the TG channel')
-        return response.json()
-
-    def check_content(self, data):
-        self.flag = False
-        start = 0
-        if len(data) > 2:
-            if self.has_header:
-                start = 1
-                header = data[0].split(',')
-                if len(header) == 2:
-                    for word in header:
-                        if word == '':
-                            self.flag = True
-                            break
-                else:
-                    self.flag = True
-
-            for i in range(start, len(data)):
-                line = data[i].split(',')
-                if len(line) == 2:
-                    for value in line:
-                        value = value.replace('\n', '')
-                        if value != '1' and value != '0':
-                            self.flag = True
-                            break
-        else:
-            self.flag = True
-        logging.debug('Checking if the file contents are correct')
 
     def file_reader(self) -> list:
         with open(self.filepath, 'r', encoding='utf-8') as file:
@@ -129,16 +32,72 @@ class Research():
             raise Exception('Incorrect data structure in the file!')
         logging.debug('Extracting file contents')
 
+    def save_file(self, data, file_name='report', extension='txt'):
+        filename = file_name + '.' + extension
+        with open(filename, 'w', encoding='utf-8') as file:
+            file.write(data)
+        logging.debug(f'Saving the data in a file named {filename}')
+        return filename
 
+class CoinStatistics():
+    def __init__(self, data):
+        self.data = data
 
-    def print_data(self, data):
-        analys = self.Analytics(data)
-        analys.fractions()
-        predictions = analys.predict_random(3)
-        last = analys.predict_last()
-        print(analys.data)
-        print(f'{analys.heads} {analys.tails}')
-        print(f'{analys.heads_perc} {analys.tails_perc}')
-        print(predictions)
-        print(last)
-        logging.debug('Printing the data')
+    def counts(self):
+        self.heads = 0
+        self.tails = 0
+        for pair in self.data:
+            if pair[0] == 1:
+                self.heads += 1
+            else:
+                self.tails += 1
+        logging.debug('Calculating the counts of heads and tails')
+
+    def fractions(self):
+        self.counts()
+        summa = self.heads + self.tails
+        self.heads_perc = round(self.heads/summa, 4)
+        self.tails_perc = 1 - self.heads_perc
+        logging.debug('Calculating the fractions of heads and tails')
+
+    def predict_random(self, number: int):
+        predictions = list()
+        for i in range(number):
+            heads = randint(0, 1)
+            pair = [heads, 1 - heads]
+            predictions.append(pair)
+        logging.debug(f'Predicting {number} tosses')
+        return predictions
+
+    def predict_last(self):
+        last_pair = self.data[-1]
+        logging.debug('Predicting based on the last toss')
+        return last_pair
+    
+    def num_to_word(self, num: int):
+        ans = num
+        if num >= 0 and num <= 10:
+            ans = config.numbers[num]
+        logging.debug('Turning numbers into words (0-10 works, the rest stay numbers)')
+        return ans
+    
+
+class TelegramNotifier():
+    def get_message(self, filename):
+        message = "The report hasn't been created due to an error"
+        if os.path.isfile(filename) and os.path.getsize(filename) > 0:
+            message = "The report has been successfully created"
+        logging.debug('Generating a message for the TG channel')
+        return message
+
+    def send_message(self, filename):
+        message = self.get_message(filename)
+
+        url = f"https://api.telegram.org/bot{token.token}/sendMessage"
+        send_inf = {
+            'chat_id': token.chat_id,
+            'text': message
+            }
+        response = requests.post(url, data=send_inf)
+        logging.debug('Sending a message to the TG channel')
+        return response.json()    
